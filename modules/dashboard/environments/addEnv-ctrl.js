2b10dddf7e884df77d4e9cf9d9acbd199017eacc
RaghebAD <ragheb.abo.dargham@gmail.com>
2017-11-06 18:54:52 +0200

    added validation to domain removed addEnvironment from main menu

--- modules/dashboard/environments/addEnv-ctrl.js
+++ modules/dashboard/environments/addEnv-ctrl.js
@@ -11,6 +11,10 @@ environmentsApp.controller('addEnvironmentCtrl', ['$scope', '$timeout', '$modal'
 	
 	$scope.wizard = {};
 	
+	//Check whether each part of the domain is not longer than 63 characters,
+	//Allow internationalized domain names
+	$scope.domainRegex= '^((?=[a-z0-9-]{1,63}\\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\\.)+[a-z]{2,63}$';
+	
 	$scope.Step1 = function () {
 		overlayLoading.show();
 		

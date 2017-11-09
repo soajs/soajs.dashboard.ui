6648121136f544faaf75ad3d8081283de92c7d10
mikehajj <mike.hajj@gmail.com>
2017-11-09 18:25:01 +0200

    minor bug fix

--- modules/dashboard/environments/addEnv-ctrl.js
+++ modules/dashboard/environments/addEnv-ctrl.js
@@ -147,16 +147,20 @@ environmentsApp.controller('addEnvironmentCtrl', ['$scope', 'overview', '$timeou
 					else{
 						$scope.form.formData = {};
 						$scope.wizard.gi = {};
+						$localStorage.addEnv = {};
 					}
 				}
 			}
 			
 			if($routeParams.portal){
-				if($scope.wizard.gi && $scope.wizard.gi.code){
+				$scope.form.formData.code = 'PORTAL';
+			}
+			else{
+				if($scope.wizard.gi && $scope.wizard.gi.code && $scope.wizard.gi.code === 'PORTAL'){
 					$scope.form.formData = {};
 					$scope.wizard.gi = {};
+					$localStorage.addEnv = {};
 				}
-				$scope.form.formData.code = 'PORTAL';
 			}
 			
 			$scope.tempFormEntries.code.onAction();

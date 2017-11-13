5ef6ad02ab223f17abbf8ac2c80e5854a7d61dff
mikehajj <mike.hajj@gmail.com>
2017-11-13 13:24:08 +0200

    removed content builder ui module.

--- config.js
+++ config.js
@@ -50,7 +50,6 @@ var modules = {
 	"develop": {
 		"dashboard": {
 			services: 'modules/dashboard/services/install.js',
-			contentBuilder: 'modules/dashboard/contentBuilder/install.js',
 			githubApp: 'modules/dashboard/gitAccounts/install.js',
 			swaggerEditorApp: 'modules/dashboard/swaggerEditor/install.js',
 			catalogs: 'modules/dashboard/catalogs/install.js',

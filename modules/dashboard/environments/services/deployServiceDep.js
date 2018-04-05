f98ab3ae7ac23a60e2e3908408d35174ade53293
mikehajj <mike.hajj@gmail.com>
2018-04-05 20:42:29 +0300

    bug fix

--- modules/dashboard/environments/services/deployServiceDep.js
+++ modules/dashboard/environments/services/deployServiceDep.js
@@ -305,7 +305,7 @@ deployService.service('deployServiceDep', ['ngDataApi', '$timeout', '$modal', '$
 				$scope.cdConfiguration[oneSrv][oneEnv].cdData.versions[version].options.gitSource = {};
 			}
 			
-			$scope.cdConfiguration[oneSrv][oneEnv].cdData.versions[version].options.gitSource.owner = oneRepo.owner.login;
+			$scope.cdConfiguration[oneSrv][oneEnv].cdData.versions[version].options.gitSource.owner = (oneRepo.owner && oneRepo.owner.login) ? oneRepo.owner.login : oneRepo.owner;
 			$scope.cdConfiguration[oneSrv][oneEnv].cdData.versions[version].options.gitSource.repo = oneRepo.name;
 			if (isKubernetes) {
 				$scope.deploymentModes = ['deployment', 'daemonset'];

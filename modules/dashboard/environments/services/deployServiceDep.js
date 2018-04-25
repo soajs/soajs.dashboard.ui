c35a61acff3d7d8eb5446a991bebb39b75fb3de2
wissamelkhoury <wissam@soajs.org>
2018-04-25 13:49:47 +0300

    minor fixes

--- modules/dashboard/environments/services/deployServiceDep.js
+++ modules/dashboard/environments/services/deployServiceDep.js
@@ -489,12 +489,17 @@ deployService.service('deployServiceDep', ['ngDataApi', '$timeout', '$modal', '$
 				//check if there port mismatch in type
 				let nodePort =0, loadBalancer=0;
 				selectedRecipe.recipe.deployOptions.ports.forEach(function (onePort) {
+					let pushed = false;
 					if (recipe){
+						pushed = true;
 						formDataRoot.custom.ports.push(onePort);
 					}
 					if(onePort.isPublished || onePort.published){
 						formDataRoot.custom.loadBalancer = true;
-						formDataRoot.custom.ports.push(onePort);
+						if(!pushed){
+							pushed = true;
+							formDataRoot.custom.ports.push(onePort);
+						}
 						if (onePort.published){
 							if (recipe) {
 								formDataRoot.custom.loadBalancer = false;

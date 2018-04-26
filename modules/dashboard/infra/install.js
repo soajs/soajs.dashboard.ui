f7935a777471eeaf802f6a266f55bbd9d06a3ceb
mikehajj <mike.hajj@gmail.com>
2018-04-26 10:33:42 +0300

    changed module's icon

--- modules/dashboard/infra/install.js
+++ modules/dashboard/infra/install.js
@@ -11,7 +11,7 @@ let infraNav = [
 		},
 		'url': '#/infra',
 		'tplPath': 'modules/dashboard/infra/directives/list.tmpl',
-		'icon': 'cog',
+		'icon': 'codepen',
 		'pillar': {
 			'name': 'development',
 			'label': translation.develop[LANG],

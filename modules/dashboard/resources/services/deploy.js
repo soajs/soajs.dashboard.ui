a8f82059289d9d3be87143ca8c7f2888ae886e2e
charleskassouf <charles.kassouf11@gmail.com>
2018-04-26 18:19:54 +0300

    update for image

--- modules/dashboard/resources/services/deploy.js
+++ modules/dashboard/resources/services/deploy.js
@@ -551,19 +551,29 @@ resourceDeployService.service('resourceDeploy', ['resourceConfiguration', '$moda
 					
 					context.recipes = [
 						{
-							"_id": "5ae07aa09fdc3e40b2ea61a1",
-							"name": "Recipe 1",
-							"type": "service",
-							"subtype": "soajs",
-							"description": "This recipe allows you to deploy a services built using the SOAJS framework",
-							"locked": true,
-							"restriction": {
-								"deployment": ["vm", "container"],
-								"driver": ["container.docker"],
-								"infra": ["azure"]
-							}
-						},
-						{
+                            "_id": "5ae07aa09fdc3e40b2ea61a1",
+                            "name": "Recipe 1",
+                            "type": "service",
+                            "subtype": "soajs",
+                            "description": "This recipe allows you to deploy a services built using the SOAJS framework",
+                            "locked": true,
+                            "restriction": {
+                                "deployment": ["vm", "container"],
+                                "driver": ["container.docker"],
+                                "infra": ["azure"]
+                            },
+                            "recipe": {
+                                "deployOptions": {
+                                    "image": {
+                                    	"override" : true,
+                                        "prefix": "provider2",
+                                        "name": "image2",
+                                        "tag": "v2"
+                                    }
+                                }
+                            }
+                        },
+                        {
 							"_id": "5ae07aa09fdc3e40b2ea61a2",
 							"name": "Recipe 2",
 							"type": "service",
@@ -574,7 +584,17 @@ resourceDeployService.service('resourceDeploy', ['resourceConfiguration', '$moda
 								"deployment": ["vm"],
 								"driver": [],
 								"infra": ["azure","aws"]
-							}
+							},
+                            "recipe": {
+                                "deployOptions": {
+                                    "image": {
+                                        "override" : true,
+                                        "prefix": "provider1",
+                                        "name": "image1",
+                                        "tag": "v1"
+                                    }
+                                }
+                            }
 						},
 						{
 							"_id": "5ae07aa09fdc3e40b2ea61a3",
@@ -587,7 +607,17 @@ resourceDeployService.service('resourceDeploy', ['resourceConfiguration', '$moda
 								"deployment": ["container"],
 								"driver": ["container.docker","container.kubernetes"],
 								"infra": []
-							}
+							},
+                            "recipe": {
+                                "deployOptions": {
+                                    "image": {
+                                        "override" : false,
+                                        "prefix": "provider1",
+                                        "name": "image1",
+                                        "tag": "v1"
+                                    }
+                                }
+                            }
 						},
 						{
 							"_id": "5ae07aa09fdc3e40b2ea61a4",
@@ -662,13 +692,14 @@ resourceDeployService.service('resourceDeploy', ['resourceConfiguration', '$moda
 		context.displayRecipeInputs = function (cb) {
 			
 			function calculateRestrictions(currentScope) {
-				
+
 				let allRecipes = currentScope.recipes;
 				let selectedRecipeId = currentScope.formData.deployOptions.recipe;
 				let selectedRecipe;
 				allRecipes.forEach(function (eachRecipe) {
 					if (eachRecipe._id === selectedRecipeId) {
 						selectedRecipe = eachRecipe;
+						context.deploymentData.selectedRecipe = selectedRecipe;
 					}
 				});
 				
@@ -682,7 +713,48 @@ resourceDeployService.service('resourceDeploy', ['resourceConfiguration', '$moda
 				}
 				currentScope.formData.deployOptions.deployConfig.type = '';
 				currentScope.formData.deployOptions.deployConfig.infra.provider = '';
-				
+
+				if (!context.formData.custom) {
+                    context.formData.custom = {}
+				}
+				if (!context.formData.custom.image) {
+                    context.formData.custom.image = {}
+				}
+				if (selectedRecipe.recipe.deployOptions && selectedRecipe.recipe.deployOptions.image.override) {
+					context.getProvidersList(() => {
+                        context.formData.custom.image.prefix = '';
+                        context.deploymentData.providers.forEach((provider) =>{
+                            if (provider.v === selectedRecipe.recipe.deployOptions.image.prefix) {
+                                context.formData.custom.image.prefix = selectedRecipe.recipe.deployOptions.image.prefix;
+                            }
+                        });
+                        context.getImagesList(selectedRecipe.recipe.deployOptions.image.prefix, () =>{
+                            context.deploymentData.images.forEach((image) =>{
+                                if (image.v === selectedRecipe.recipe.deployOptions.image.name) {
+                                    context.formData.custom.image.name = image.v
+                                }
+                            });
+                            if (context.deploymentData.images.length === 0) {
+                                context.deploymentData.imageVersions = [];
+                            } else {
+                                context.getVersionsList(selectedRecipe.recipe.deployOptions.image.name, () =>{
+                                    context.deploymentData.imageVersions.forEach((version) =>{
+                                        if (version.v === selectedRecipe.recipe.deployOptions.image.tag) {
+                                            context.formData.custom.image.tag = version.v
+                                        }
+                                    });
+                                });
+                            }
+                        })
+					});
+				} else {
+                    context.formData.custom.image.prefix = '';
+                    context.formData.custom.image.name = '';
+                    context.formData.custom.image.tag = '';
+				}
+
+
+
 				let allDeployments = ["container", "vm"]; // enable all if no rest or empty rest & ! manual
 				let allInfra = ["azure", "aws", "google"];
 				if (!selectedRecipe) {
@@ -700,7 +772,7 @@ resourceDeployService.service('resourceDeploy', ['resourceConfiguration', '$moda
 				
 				if (currentScope.deploymentData.selectedRestrictionsDep.length === 1) { // force select deployment technology iff one is available
 					currentScope.formData.deployOptions.deployConfig.type = currentScope.deploymentData.selectedRestrictionsDep[0];
-					context.onDeploymentTechnologySelect();
+					context.onDeploymentTechnologySelect(selectedRecipe);
 				}
 			}
 			
@@ -1022,51 +1094,85 @@ resourceDeployService.service('resourceDeploy', ['resourceConfiguration', '$moda
 				'l': 'Asia Pacific (Sydney)'
 			}, {v: 'sa-east-1', 'l': 'South America (São Paulo)'}];
 		};
+
 		context.getVmSizesList = function () {
 			context.deploymentData.vmSize = [
 				{v : 'smallSize1', l:"Small Size 1"},
 				{v : 'mediumSize2', l:"Medium Size 2"}
 			];
 		};
+
 		context.getDisksList = function () {
 			context.deploymentData.disk = [
 				{v : 'none', l:"None"}
 			];
 		};
-		context.getProvidersList = function () {
+
+		context.getProvidersList = function (cb) {
+			context.deploymentData.providers = [];
 			context.deploymentData.providers = [
-				{v : 'provider1', l:"Provider 1 Samir"}
+				{v : "provider1", l: "Provider 1"},
+				{v: "provider2", l: "Provider 2"}
 			];
+			if (cb) {
+				cb();
+			}
 		};
-		context.getImagesList = function (providerName) {
-			context.deploymentData.images = {
-				"provider1" : [
-					{v : 'image1', l:"Image 1 example"}
-				]
-			};
+
+		context.getImagesList = function (providerName, cb) {
+			let values = {
+                "provider1" : [
+                    {v : 'image1', l:"Image 1 example"},
+                ],
+                "provider2" : [
+                    {v : 'image2', l:"Image 2 example"},
+                ]
+            };
+			if (values[providerName]  === undefined) {
+                context.deploymentData.images = [];
+			} else {
+                context.deploymentData.images = values[providerName];
+			}
+
+			if(cb){
+				cb();
+			}
 		};
-		context.getVersionsList = function (imageName) {
-			context.deploymentData.imageVersions = {
-				"image1" : [
-					{v : 'v1', l:"Version 1 - Alfa"}
-				]
-			};
-		};
+
+        context.getVersionsList = function (imageName, cb) {
+            let values = {
+                "image1": [
+                    {v: 'v1', l: "Version 1 - Alfa"}
+                ],
+                "image2": [
+                    {v: 'v2', l: "Version 2 - Alfa"}
+                ]
+            };
+            if (values[imageName] === undefined) {
+                context.deploymentData.imageVersions = [];
+            }
+            context.deploymentData.imageVersions = values[imageName];
+            if (cb) {
+                cb();
+            }
+        };
 		
 		// listeners
 		let vmStuffAreLoaded = false;
-		context.onDeploymentTechnologySelect = function () {
+		context.onDeploymentTechnologySelect = function (selectedRecipe) {
 			
 			if(!vmStuffAreLoaded){
 				if(context.formData.deployOptions.deployConfig.type === 'vm'){
 					context.getRegionsList();
 					context.getVmSizesList();
 					context.getDisksList();
-					context.getProvidersList();
+                    context.getProvidersList();
+					//add checks if override true
+					console.log( context ); // ToDelete #2del
 				}
 				vmStuffAreLoaded = true;
 			}
-		}
+		};
 		context.onAuthTypeChange = function () {
 			if(context.formData.deployOptions.deployConfig.vmConfiguration.adminAccess.isPassword){
 				context.formData.deployOptions.deployConfig.vmConfiguration.adminAccess.token = '';
@@ -1092,10 +1198,10 @@ resourceDeployService.service('resourceDeploy', ['resourceConfiguration', '$moda
 				context.displayRecipeInputs(cb);
 			});
 		}
-		
+
 		if (cb && typeof cb === 'function')
 			return cb();
-	}
+	};
 	
 	return {
 		'buildDeployForm': buildDeployForm

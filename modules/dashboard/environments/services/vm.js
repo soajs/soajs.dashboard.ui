59901c66290bd14170029386f4a09d5f5b262af1
mikehajj <mike.hajj@gmail.com>
2018-05-03 13:05:07 +0300

    completed ui for vm management

--- modules/dashboard/environments/services/vm.js
+++ modules/dashboard/environments/services/vm.js
@@ -1,12 +1,7 @@
 "use strict";
 var vmServices = soajsApp.components;
 vmServices.service('vmSrv', [ 'ngDataApi', '$timeout', '$modal', function (ngDataApi, $timeout, $modal) {
-	/**
-	 * Service Functions
-	 * @param currentScope
-	 * @param env
-	 * @param noPopulate
-	 */
+	
 	function listServices(currentScope) {
 		var env = currentScope.envCode.toLowerCase();
 		
@@ -25,148 +20,99 @@ vmServices.service('vmSrv', [ 'ngDataApi', '$timeout', '$modal', function (ngDat
 					currentScope.displayAlert('danger', error.message);
 				}
 				else {
-					let response = {
-						azure: {
-							label: "My Azure Provider",
-							list: [
-								{
-									"type": "vm",
-									"name": "mikemongovm",
-									"id": "mikemongovm",
-									"labels": {
-										"soajs.content": "true",
-										"soajs.env.code": "dashboard",
-										"soajs.service.technology": "vm",
-										"soajs.infra.id": "5ae9e5cd55dc7960e796823b",
-										"soajs.resource.id": "5ae9f082e371a7632ded1243",
-										"soajs.image.prefix": "Canonical",
-										"soajs.image.name": "UbuntuServer",
-										"soajs.image.tag": "17.10",
-										"soajs.service.name": "mikemongovm",
-										"soajs.service.label": "mikemongovm",
-										"soajs.service.type": "cluster",
-										"soajs.service.subtype": "mongo"
-									},
-									"ports": [
-										{
-											"protocol": "tcp",
-											"target": 27017,
-											"published": 27017
-										},
-										{
-											"protocol": "tcp",
-											"target": 22,
-											"published": 22
-										}
-									],
-									"voluming": {},
-									"tasks": [
-										{
-											"id": "mikemongovm",
-											"name": "mikemongovm",
-											"status": {
-												"state": "succeeded",
-												"ts": 1525338149668
-											},
-											"ref": {
-												"os": {
-													"type": "Linux",
-													"diskSizeGB": 30
-												}
-											}
-										}
-									],
-									"env": [],
-									"servicePortType": "nodePort",
-									"ip": "xxx-xxx-xxx-xxx"
-								},
-								{
-									"type": "vm",
-									"name": "mikemongovm2",
-									"id": "mikemongovm2",
-									"labels": {
-										"soajs.content": "true",
-										"soajs.env.code": "dashboard",
-										"soajs.service.technology": "vm",
-										"soajs.infra.id": "5ae9e5cd55dc7960e796823b",
-										"soajs.resource.id": "5ae9f082e371a7632ded1243",
-										"soajs.image.prefix": "Canonical",
-										"soajs.image.name": "UbuntuServer",
-										"soajs.image.tag": "17.10",
-										"soajs.service.name": "mikemongovm2",
-										"soajs.service.label": "mikemongovm2",
-										"soajs.service.type": "cluster",
-										"soajs.service.subtype": "mongo"
-									},
-									"ports": [
-										{
-											"protocol": "tcp",
-											"target": 27017,
-											"published": 27017
-										},
-										{
-											"protocol": "tcp",
-											"target": 22,
-											"published": 22
-										}
-									],
-									"voluming": {},
-									"tasks": [
-										{
-											"id": "mikemongovm",
-											"name": "mikemongovm",
-											"status": {
-												"state": "succeeded",
-												"ts": 1525338149668
-											},
-											"ref": {
-												"os": {
-													"type": "Linux",
-													"diskSizeGB": 30
-												}
-											}
-										}
-									],
-									"env": [],
-									"servicePortType": "nodePort",
-									"ip": "xxx-xxx-xxx-xxx"
-								}
-							]
-						}
-					};
+					// let response = {
+					// 	azure: {
+					// 		label: "My Azure Provider",
+					// 		list: [
+					// 			{
+					// 				"type": "vm",
+					// 				"name": "mikemongovm",
+					// 				"id": "mikemongovm",
+					// 				"labels": {
+					// 					"soajs.content": "true",
+					// 					"soajs.env.code": "dashboard",
+					// 					"soajs.service.technology": "vm",
+					// 					"soajs.infra.id": "5ae9e5cd55dc7960e796823b",
+					// 					"soajs.resource.id": "5ae9f082e371a7632ded1243",
+					// 					"soajs.image.prefix": "Canonical",
+					// 					"soajs.image.name": "UbuntuServer",
+					// 					"soajs.image.tag": "17.10",
+					// 					"soajs.service.name": "mikemongovm",
+					// 					"soajs.service.label": "mikemongovm",
+					// 					"soajs.service.type": "cluster",
+					// 					"soajs.service.subtype": "mongo",
+					// 					"soajs.service.vm.location": "eastus",
+					// 					"soajs.service.vm.group": "DASHBOARD",
+					// 					"soajs.service.vm.size": "Standard_A1"
+					//
+					// 				},
+					// 				"ports": [
+					// 					{
+					// 						"protocol": "tcp",
+					// 						"target": 27017,
+					// 						"published": 27017
+					// 					},
+					// 					{
+					// 						"protocol": "tcp",
+					// 						"target": 22,
+					// 						"published": 22
+					// 					}
+					// 				],
+					// 				"voluming": {},
+					// 				"tasks": [
+					// 					{
+					// 						"id": "mikemongovm",
+					// 						"name": "mikemongovm",
+					// 						"status": {
+					// 							"state": "succeeded",
+					// 							"ts": 1525338149668
+					// 						},
+					// 						"ref": {
+					// 							"os": {
+					// 								"type": "Linux",
+					// 								"diskSizeGB": 30
+					// 							}
+					// 						}
+					// 					}
+					// 				],
+					// 				"env": [],
+					// 				"servicePortType": "nodePort",
+					// 				"ip": "xxx-xxx-xxx-xxx"
+					// 			}
+					// 		]
+					// 	}
+					// };
 					
 					currentScope.vms = response;
-					console.log(currentScope.vms);
 				}
 			});
 		}
 	}
 	
 	function deleteService(currentScope, service) {
-		console.log(service);
-		// var params = {
-		// 	env: currentScope.envCode,
-		// 	namespace: service.namespace,
-		// 	serviceId: service.id,
-		// 	mode: ((service.labels && service.labels['soajs.service.mode']) ? service.labels['soajs.service.mode'] : ''),
-		// 	name: service.name
-		// };
-		//
-		// overlayLoading.show();
-		// getSendDataFromServer(currentScope, ngDataApi, {
-		// 	method: 'delete',
-		// 	routeName: '/dashboard/cloud/services/delete',
-		// 	params: params
-		// }, function (error, response) {
-		// 	overlayLoading.hide();
-		// 	if (error) {
-		// 		currentScope.displayAlert('danger', error.message);
-		// 	}
-		// 	else {
-		// 		currentScope.displayAlert('success', 'Service deleted successfully');
-		// 		currentScope.listServices();
-		// 	}
-		// });
+		let params = {
+			"env": currentScope.envCode,
+			"serviceId": service.id,
+			"infraAccountId": service.labels['soajs.infra.id'],
+			"location": service.labels['soajs.service.vm.location'],
+			"technology" : service.labels['soajs.service.technology']
+		};
+
+		overlayLoading.show();
+		getSendDataFromServer(currentScope, ngDataApi, {
+			method: 'delete',
+			routeName: '/dashboard/cloud/services/delete',
+			params: params
+		}, function (error) {
+			overlayLoading.hide();
+			if (error) {
+				currentScope.displayAlert('danger', error.message);
+			}
+			else {
+				currentScope.displayAlert('success', 'Service deleted successfully');
+				listServices(currentScope);
+			}
+		});
 	}
 	
 	function inspectService(currentScope, service) {
@@ -193,7 +139,29 @@ vmServices.service('vmSrv', [ 'ngDataApi', '$timeout', '$modal', function (ngDat
 	}
 	
 	function maintenanceService(currentScope, service, operation) {
-		console.log(service);
+		let params = {
+			"env": currentScope.envCode,
+			"name": service.name,
+			"location": service.labels['soajs.service.vm.location'],
+			"infraAccountId": service.labels['soajs.infra.id'],
+			"operation": operation
+		};
+		
+		overlayLoading.show();
+		getSendDataFromServer(currentScope, ngDataApi, {
+			method: 'post',
+			routeName: '/dashboard/cloud/vm/maintenance',
+			params: params
+		}, function (error) {
+			overlayLoading.hide();
+			if (error) {
+				currentScope.displayAlert('danger', error.message);
+			}
+			else {
+				currentScope.displayAlert('success', 'Service deleted successfully');
+				listServices(currentScope);
+			}
+		});
 	}
 	
 	return {

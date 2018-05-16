0247bdcc0bc1f8c1581edbfe88927919bae68c52
charleskassouf <charles.kassouf11@gmail.com>
2018-05-16 11:22:06 +0300

    adding delete tuggle and list to resource

--- modules/dashboard/resources/controller.js
+++ modules/dashboard/resources/controller.js
@@ -11,61 +11,153 @@ resourcesApp.controller('resourcesAppCtrl', ['$scope', '$http', '$timeout', '$mo
 		envPlatform: '',
 	};
 	constructModulePermissions($scope, $scope.access, resourcesAppConfig.permissions);
+
+    $scope.listResources = function (isInBetween, cb) {
+
+        commonUtils.listResources($scope, function (response) {
+            $scope.resources = {list: response};
+            $scope.resources.original = angular.copy($scope.resources.list); //keep a copy of the original resources records
+
+            if ($scope.deployedServices) {
+                markDeployed();
+            }
+
+            groupByType();
+
+            function groupByType() {
+                $scope.resources.types = {};
+                $scope.resources.list.forEach(function (oneResource) {
+                    if (!$scope.resources.types[oneResource.type]) {
+                        $scope.resources.types[oneResource.type] = {};
+                    }
+                    if (!$scope.resources.types[oneResource.type][oneResource.category]) {
+                        $scope.resources.types[oneResource.type][oneResource.category] = [];
+                    }
+
+                    if (oneResource.created === $scope.envCode.toUpperCase()) {
+                        oneResource.allowEdit = true;
+                    }
+
+                    if (oneResource.name === 'dash_cluster') {
+                        oneResource.sensitive = true;
+                    }
+
+                    $scope.resources.types[oneResource.type][oneResource.category].push(oneResource);
+                });
+            }
+
+            function markDeployed() {
+                $scope.resources.list.forEach(function (oneResource) {
+                    if ($scope.deployConfig && $scope.deployConfig[$scope.envCode.toUpperCase()]) {
+                        if ($scope.deployConfig[$scope.envCode.toUpperCase()][oneResource.name]) {
+                            var resourceConfig = $scope.deployConfig[$scope.envCode.toUpperCase()][oneResource.name];
+
+                            if (!resourceConfig.deploy) return;
+                            if (!resourceConfig.options || !resourceConfig.options.recipe) return;
+
+                            oneResource.canBeDeployed = true;
+                            oneResource.deployOptions = $scope.deployConfig[$scope.envCode.toUpperCase()][oneResource.name].options;
+
+                            if ($scope.deployConfig[$scope.envCode.toUpperCase()][oneResource.name].status && oneResource.deployOptions.deployConfig.type === 'vm') {
+                                if (!oneResource.instance) {
+                                    oneResource.isDeployed = true;
+                                    oneResource.instance = {
+                                        id: oneResource.name
+                                    };
+                                    oneResource.status = $scope.deployConfig[$scope.envCode.toUpperCase()][oneResource.name].status;
+                                }
+                            }
+                        }
+                    }
+
+                    for (var i = 0; i < $scope.deployedServices.length; i++) {
+                        if ($scope.deployedServices[i].labels && $scope.deployedServices[i].labels['soajs.resource.id'] === oneResource._id.toString()) {
+                            oneResource.isDeployed = true;
+                            oneResource.instance = $scope.deployedServices[i];
+                        }
+                        else if ($scope.deployedServices[i].name === oneResource.name && $scope.deployedServices[i].labels["soajs.service.technology"] === "vm") {
+                            oneResource.isDeployed = true;
+                            oneResource.instance = $scope.deployedServices[i];
+                            oneResource.canBeDeployed = true;
+                            if ($scope.deployConfig && $scope.deployConfig[$scope.envCode.toUpperCase()] && $scope.deployConfig[$scope.envCode.toUpperCase()][oneResource.name]) {
+                                oneResource.deployOptions = $scope.deployConfig[$scope.envCode.toUpperCase()][oneResource.name].options;
+                                oneResource.status = $scope.deployConfig[$scope.envCode.toUpperCase()][oneResource.name].status;
+                            }
+                        }
+                    }
+                });
+            }
+
+            if (cb) return cb();
+        });
+
+    };
+
+    $scope.deleteResource = function (resource) {
+    	let params = {
+    		resource : resource,
+		};
+
+        if (resource.isDeployed && resource.instance && resource.instance.id) {
+            params.env = $scope.envCode;
+            params.serviceId = resource.instance.id;
+            params.name = resource.instance.name;
+
+            if (resource && resource.instance && resource.instance.labels && resource.instance.labels['soajs.service.mode'] && resource.instance.labels['soajs.service.technology'] !== 'vm') {
+                params.mode = resource.instance.labels['soajs.service.mode'];
+            }
+
+            if (resource.instance.labels && resource.instance.labels['soajs.service.technology'] === 'vm') {
+                params.infraAccountId = resource.instance.labels['soajs.infra.id'];
+                params.location = resource.instance.labels['soajs.service.vm.location'];
+                params.technology = resource.instance.labels['soajs.service.technology'];
+                delete params.mode;
+            }
+        }
+
+        commonUtils.deleteResources($scope, params, function () {
+            $scope.displayAlert('success', 'Resource deleted successfully');
+            $scope.listResources();
+        });
+    };
+
+    $scope.togglePlugResource = function (resource, plug) {
+        var resourceRecord = {};
+        //get the original resource record
+        for (var i = 0; i < $scope.resources.original.length; i++) {
+            if ($scope.resources.original[i]._id === resource._id) {
+                resourceRecord = angular.copy($scope.resources.original[i]);
+                break;
+            }
+        }
+
+        var resourceId = resourceRecord._id;
+        delete resourceRecord._id;
+        delete resourceRecord.created;
+        delete resourceRecord.author;
+        delete resourceRecord.permission;
+        resourceRecord.plugged = plug;
+
+        let params = {
+            resourceId : resourceId,
+            resourceRecord : resourceRecord
+		};
+
+        commonUtils.togglePlugResource($scope, params, function () {
+            $scope.displayAlert('success', 'Resource updated successfully');
+            $scope.listResources(false);
+        });
+    };
+
+    $scope.upgradeAll = function () {
+        commonUtils.upgradeAll($scope, function () {
+            $scope.displayAlert('success', "Resources have been upgraded to the latest version.");
+            $scope.listResources();
+		})
+    };
 	
-	// $scope.listResources = function () {
-	//
-	// };
-	
-	$scope.listResources = function (isInBetween, cb) {
-		function groupByType() {
-			$scope.resources.types = {};
-			$scope.resources.list.forEach(function (oneResource) {
-				if (!$scope.resources.types[oneResource.type]) {
-					$scope.resources.types[oneResource.type] = {};
-				}
-				if (!$scope.resources.types[oneResource.type][oneResource.category]) {
-					$scope.resources.types[oneResource.type][oneResource.category] = [];
-				}
-				
-				if (oneResource.created === $scope.envCode.toUpperCase()) {
-					oneResource.allowEdit = true;
-				}
-				
-				if (oneResource.name === 'dash_cluster') {
-					oneResource.sensitive = true;
-				}
-				
-				$scope.resources.types[oneResource.type][oneResource.category].push(oneResource);
-			});
-		}
-		
-		commonUtils.listResources($scope, function (response) {
-			$scope.resources = {list: response};
-			$scope.resources.original = angular.copy($scope.resources.list); //keep a copy of the original resources records
-			
-			if ($scope.deployedServices) {
-				$scope.markDeployed();
-			}
-			
-			groupByType();
-			
-			if (cb) return cb();
-		});
-		
-	};
-	
-	$scope.addResource = function () {
-		
-	};
-	
-	$scope.deleteResource = function (id) {
-		
-	};
-	
-	$scope.deleteResource = function () {
-		
-	};
-	
+	$scope.addResource = function () {};
+
 	//start here
 	if ($scope.access.list) {
 		injectFiles.injectCss("modules/dashboard/resources/resources.css");
--- modules/dashboard/resources/services/commonUtils.js
+++ modules/dashboard/resources/services/commonUtils.js
@@ -1,11 +1,12 @@
 "use strict";
 var commonUtilsService = soajsApp.components;
 commonUtilsService.service('commonUtils', ['ngDataApi', function (ngDataApi) {
+
 	function listResources($scope, cb) {
 		overlayLoading.show();
 		getSendDataFromServer($scope, ngDataApi, {
 			method: 'get',
-			routeName: '/dashboard/resources/list',
+			routeName: '/dashboard/resources/list', // must edit the path
 			params: {
 				env: $scope.envCode
 			}
@@ -19,9 +20,73 @@ commonUtilsService.service('commonUtils', ['ngDataApi', function (ngDataApi) {
 			}
 		});
 	}
-	
+
+	function deleteResource ($scope, params, cb) {
+        overlayLoading.show();
+        getSendDataFromServer($scope, ngDataApi, {
+            method: 'delete',
+            routeName: '/dashboard/resources/delete', // must edit the path
+            params: {
+                env: $scope.envCode.toUpperCase(),
+                id: params.resource._id
+            }
+        }, function (error) {
+            overlayLoading.hide();
+            if (error) {
+                $scope.displayAlert('danger', error.message);
+            }
+            else {
+               return cb();
+            }
+        });
+	}
+
+    function togglePlugResource ($scope, params, cb) {
+        overlayLoading.show();
+        getSendDataFromServer($scope, ngDataApi, {
+            method: 'put',
+            routeName: '/dashboard/resources/update', // update the path if any
+            params: {
+                id: params.resourceId,
+                env: $scope.envCode.toUpperCase(),
+            },
+            data: { resource: params.resourceRecord }
+        }, function (error) {
+
+            overlayLoading.hide();
+            if (error) {
+                $scope.displayAlert('danger', error.message);
+            }
+            else {
+            	return cb();
+            }
+        });
+    }
+
+    function upgradeAll ($scope, cb) {
+        overlayLoading.show();
+        getSendDataFromServer($scope, ngDataApi, {
+            method: 'get',
+            routeName: '/dashboard/resources/upgrade',
+            params: {
+                env: $scope.envCode
+            }
+        }, function (error, response) {
+            overlayLoading.hide();
+            if (error) {
+                $scope.displayAlert('danger', error.message);
+            }
+            else {
+                return cb();
+            }
+        });
+    }
+
 	return {
-		listResources
+		listResources,
+        deleteResource,
+        togglePlugResource,
+        upgradeAll
 	};
 	
 }]);

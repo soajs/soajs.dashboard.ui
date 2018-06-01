ed7d0ed6518775f1606e555b145e6d781df84643
wissamelkhoury <wissam@soajs.org>
2018-06-01 17:18:26 +0300

    Fix: if input.data is object -> parse to string before sending to driver

--- modules/dashboard/secrets/services/secretsService.js
+++ modules/dashboard/secrets/services/secretsService.js
@@ -1,17 +1,17 @@
 "use strict";
 var secretsApp = soajsApp.components;
 secretsApp.service('secretsService', ['ngDataApi', '$timeout', '$window', function (ngDataApi, $timeout, $window) {
-	
+
 	function addSecret($scope, $modalInstance, currentScope, actions, extraInputs, data, cb) {
-		
+
 		$scope.textMode = false;
-		
+
 		let formConfig = angular.copy(secretsAppConfig.form.addSecret);
-		
+
 		if(extraInputs && Array.isArray(extraInputs) && extraInputs.length > 0){
 			formConfig = formConfig.concat(extraInputs);
 		}
-		
+
 		if(!actions){
 			actions = [
 				{
@@ -33,7 +33,7 @@ secretsApp.service('secretsService', ['ngDataApi', '$timeout', '$window', functi
 				}
 			]
 		}
-		
+
 		let options = {
 			timeout: $timeout,
 			entries: formConfig,
@@ -41,7 +41,7 @@ secretsApp.service('secretsService', ['ngDataApi', '$timeout', '$window', functi
 			name: 'newSecret',
 			actions: actions
 		};
-		
+
 		$scope.save = function(formData, cb){
 			$scope.$valid = true;
 			let input = {
@@ -50,26 +50,26 @@ secretsApp.service('secretsService', ['ngDataApi', '$timeout', '$window', functi
 				type: 'Opaque',
 				namespace: currentScope.namespaceConfig.namespace
 			};
-			
+
 			if (!input.data && formData.file) {
 				delete $scope.editor;
 				delete formData.secretData;
 				input.data = formData.file;
 				input.datatype = "file";
 			}
-			
+
 			if (formData.secretData) {
 				input.data = formData.secretData;
 				input.datatype = "text";
 				delete formData.file;
 			}
-			
+
 			if (!input.data && $scope.editor) {
 				input.data = $scope.editor.ngModel;
 				input.datatype = "editor";
 				delete formData.file;
 			}
-			
+
 			if(input.datatype ==='editor'){
 				try{
 					let x = JSON.parse(input.data);
@@ -80,14 +80,18 @@ secretsApp.service('secretsService', ['ngDataApi', '$timeout', '$window', functi
 					return false;
 				}
 			}
-			
+
 			if (!input.data || input.data === "" || ((input.data === "{}" || (typeof input.data === 'object' && Object.keys(input.data).length === 0)) && !formData.secretData && !formData.file)) {
 				$window.alert("Provide a value for your secret to proceed!");
 				$scope.$valid = false;
 				return false;
 			}
-			
+
 			if($scope.$valid && $modalInstance){
+				if (typeof input.data === "object") {
+					input.data = JSON.stringify(input.data);
+				}
+				
 				getSendDataFromServer(currentScope, ngDataApi, {
 					method: 'post',
 					routeName: '/dashboard/secrets/add',
@@ -103,12 +107,12 @@ secretsApp.service('secretsService', ['ngDataApi', '$timeout', '$window', functi
 					}
 				});
 			}
-			
+
 			if(cb && typeof cb === 'function'){
 				return cb(input);
 			}
 		};
-		
+
 		function enableTextMode(textMode, editor) {
 			$scope.textMode = textMode;
 			if (textMode) {
@@ -119,48 +123,48 @@ secretsApp.service('secretsService', ['ngDataApi', '$timeout', '$window', functi
 				$scope.editor = editor;
 			}
 		}
-		
+
 		formConfig[1].tabs[0].entries[0].onAction = function (id, value, form) {
 			enableTextMode(value, form.entries[1].tabs[0].entries[1]);
 			delete form.formData.secretData;
 		};
-		
+
 		formConfig[1].tabs[0].onAction = function (id, value, form) {
 			delete form.formData.file;
 		};
-		
+
 		formConfig[1].tabs[1].onAction = function (id, value, form) {
 			form.formData.secretData = "";
 			if ($scope.editor) {
 				$scope.editor.ngModel = "{}";
 			}
 		};
-		
+
 		$scope.showContent = function (id, value, form) {
 			if (!form.formData.file) {
 				form.formData.file = value;
 			}
 		};
-		
+
 		$scope.removFile = function (form) {
 			if (form && form.formData) {
 				delete form.formData.file
 			}
 		};
-		
+
 		buildForm($scope, $modalInstance, options, function () {
 			$scope.editor = $scope.form.entries[1].tabs[0].entries[1];
-			
+
 			if(data && data.namespace && extraInputs && Array.isArray(extraInputs) && extraInputs.length > 0){
 				$scope.form.formData.namespace = data.namespace;
 			}
-			
+
 			if(cb && typeof cb === 'function'){
 				return cb();
 			}
 		});
 	}
-	
+
 	return {
 		'addSecret': addSecret
 	}

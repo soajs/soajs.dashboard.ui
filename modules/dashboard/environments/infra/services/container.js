4978d33b824ee528c56eeafb16816d661a45a6f0
wissamelkhoury <wissam@soajs.org>
2018-07-06 15:24:56 +0300

    Added functions to update currentDeployer when attaching/detaching tech

--- modules/dashboard/environments/infra/services/container.js
+++ modules/dashboard/environments/infra/services/container.js
@@ -1,10 +1,10 @@
 "use strict";
 var platformContainerServices = soajsApp.components;
 platformContainerServices.service('platformCntnr', ['ngDataApi', '$timeout', '$modal', '$cookies', '$window', 'deploymentSrv', '$localStorage', function (ngDataApi, $timeout, $modal, $cookies, $window, deploymentSrv, $localStorage) {
-	
+
 	function openContainerWizard(currentScope){
 		delete $localStorage.envType;
-		
+
 		currentScope.wizard = {
 			gi: {
 				code: currentScope.envCode
@@ -32,7 +32,7 @@ platformContainerServices.service('platformCntnr', ['ngDataApi', '$timeout', '$m
 							deploymentSrv.handleFormData(currentScope, formData);
 							let postedData = angular.copy(currentScope.wizard);
 							delete postedData.template;
-							
+
 							getSendDataFromServer(currentScope, ngDataApi, {
 								"method": "post",
 								"routeName": "/dashboard/environment/platforms/attach",
@@ -49,8 +49,9 @@ platformContainerServices.service('platformCntnr', ['ngDataApi', '$timeout', '$m
 								else {
 									currentScope.displayAlert('success', "Container Technology attached to environment successfully, changes will appear soon.");
 									delete currentScope.wizard;
+									currentScope.currentDeployer.type = 'container';
 									currentScope.containerWizard = false;
-									
+									getEnvRecord(currentScope);
 									$timeout(() => {
 										checkAttachContainerProgress(currentScope, true);
 									}, 1000);
@@ -73,7 +74,7 @@ platformContainerServices.service('platformCntnr', ['ngDataApi', '$timeout', '$m
 			buildForm(currentScope, null, options);
 		});
 	}
-	
+
 	function detachContainerTechnology(currentScope) {
 		$modal.open({
 			templateUrl: "detachContainerTechnology.tmpl",
@@ -82,9 +83,9 @@ platformContainerServices.service('platformCntnr', ['ngDataApi', '$timeout', '$m
 			keyboard: true,
 			controller: function ($scope, $modalInstance) {
 				fixBackDrop();
-				
+
 				$scope.proceed = function(){
-					
+					overlayLoading.show();
 					getSendDataFromServer(currentScope, ngDataApi, {
 						"method": "delete",
 						"routeName": "/dashboard/environment/platforms/detach",
@@ -92,34 +93,38 @@ platformContainerServices.service('platformCntnr', ['ngDataApi', '$timeout', '$m
 							"env": currentScope.envCode
 						}
 					}, function (error) {
+						overlayLoading.hide();
 						if (error) {
 							currentScope.displayAlert('danger', error.code, true, 'dashboard', error.message);
 						}
 						else {
 							$modalInstance.close();
+							currentScope.currentDeployer.type = 'manual';
 							currentScope.displayAlert('success', "Container Technology has been detached from this environment.");
+							getEnvRecord(currentScope);
 							$timeout(() => {
 								checkAttachContainerProgress(currentScope);
 							}, 1000);
 						}
 					});
-					
+
 				};
-				
+
 				$scope.cancel = function(){
 					$modalInstance.close();
 				};
 			}
 		});
 	}
-	
+
 	function checkAttachContainerProgress(currentScope, autoRefresh){
 		currentScope.getEnvPlatform(true);
-		
+
 		if(autoRefresh){
 			let autoRefreshTimeoutProgress = $timeout(() => {
 				if(!currentScope.environment.pending){
 					$timeout.cancel(autoRefreshTimeoutProgress);
+					getEnvRecord(currentScope);
 				}
 				else{
 					checkAttachContainerProgress(currentScope, autoRefresh);
@@ -127,7 +132,53 @@ platformContainerServices.service('platformCntnr', ['ngDataApi', '$timeout', '$m
 			}, 10 * 1000);
 		}
 	}
-	
+
+	function getEnvRecord(currentScope) {
+		getSendDataFromServer(currentScope, ngDataApi, {
+			"method": "get",
+			"routeName": "/dashboard/environment",
+			"params": {
+				"code": currentScope.envCode
+			}
+		}, function (error, response) {
+			if (error) {
+				currentScope.displayAlert('danger', error.code, true, 'dashboard', error.message);
+			}
+			else {
+				putMyEnv(response);
+			}
+		});
+
+		function putMyEnv(record) {
+			var data = {
+				"_id": record._id,
+				"code": record.code,
+				"sensitive": record.sensitive,
+				"domain": record.domain,
+				"profile": record.profile,
+				"sitePrefix": record.sitePrefix,
+				"apiPrefix": record.apiPrefix,
+				"description": record.description,
+				"deployer": record.deployer,
+				"pending": record.pending,
+				"error": record.error
+			};
+			for(let container in data.deployer.container){
+				for(let driver in data.deployer.container[container]){
+					if(data.deployer.container[container][driver].auth && data.deployer.container[container][driver].auth.token){
+						delete data.deployer.container[container][driver].auth.token;
+					}
+				}
+			}
+			$cookies.putObject('myEnv', data, { 'domain': interfaceDomain });
+
+			$timeout(() => {
+				currentScope.switchEnvironment(data);
+				currentScope.rebuildMenus(function(){});
+			}, 100);
+		}
+	}
+
 	return {
 		'openContainerWizard': openContainerWizard,
 		'detachContainerTechnology': detachContainerTechnology,

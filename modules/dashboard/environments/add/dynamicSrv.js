933665b8c864761339ac2c97058f1880573847ff
mikehajj <mike.hajj@gmail.com>
2018-08-22 19:17:42 +0300

    fixed issue when unwanted vm layer shows up. fixed issue when pressing back in wizard and getting disrupted resource deployment configuration.

--- modules/dashboard/environments/add/dynamicSrv.js
+++ modules/dashboard/environments/add/dynamicSrv.js
@@ -624,7 +624,7 @@ dynamicServices.service('dynamicSrv', ['ngDataApi', '$timeout', '$modal', '$loca
 					if (currentScope.wizard.onboardNames) {
                         resource.scope.onboardNames = currentScope.wizard.onboardNames;
 					}
-					// TODO
+					
 					if(currentScope.wizard.vms || currentScope.wizard.vmOnBoard){
 						if(!resource.scope.mainData.deploymentData){
 							resource.scope.mainData.deploymentData = {}
@@ -801,8 +801,9 @@ dynamicServices.service('dynamicSrv', ['ngDataApi', '$timeout', '$modal', '$loca
 							oneTemplateSecret.uid = "from-template-" + oneTemplateSecret.name.toLowerCase();
 						});
 					}
-
-					resourceDeploy.buildDeployForm(resource.scope, resource.scope, null, record, 'add', settings, () => {
+					
+					let alreadyFilledFormData = record;
+					resourceDeploy.buildDeployForm(resource.scope, resource.scope, null, angular.copy(record), 'add', settings, () => {
 						if(currentScope.wizard.template.content.deployments.resources[key].deploy){
 							resource.scope.hideDeployButton = true;
 							if(isKubernetes){
@@ -868,39 +869,70 @@ dynamicServices.service('dynamicSrv', ['ngDataApi', '$timeout', '$modal', '$loca
 						}
 						let entries = [];
 						buildDynamicForm(resource.scope, entries, () => {
-
-							if(resource.scope.formData.deployOptions && resource.deployOptions){
-								resource.scope.formData.deployOptions.deployConfig = resource.deployOptions.deployConfig;
-								let deployConfig = resource.scope.formData.deployOptions.deployConfig;
-								if(deployConfig && deployConfig.type ==='vm'){
-									resource.scope.onDeploymentTechnologySelect();
-								}
-							}
-
 							let element = angular.element(document.getElementById("resource_" + key));
 							element.append("<form name=\"addEditResource\" id=\"addEditResource\"><div ng-include=\"'modules/dashboard/resources/directives/resource.tmpl'\"></div></form>");
 							$compile(element.contents())(resource.scope);
-
-							if(currentScope.wizard.template.content.deployments.resources[key].deploy){
-								setTimeout(() => {
-									resource.scope.updateDeploymentName(record.name);
-								}, 200);
-							}
-
+							
 							resource.scope.$watch("addEditResource.$invalid", function($invalid){
 								resource.formIsInvalid = $invalid;
 							});
-
+							
 							resource.scope.$watch('catalogConflictingPorts', (value) => {
 								currentScope.loadingDynamicSection = value && value !== '';
 							});
-
-							counter++;
-							if (counter < entriesNames.length) {
-								buildMyForms(counter, cb);
+							
+							if(currentScope.wizard.template.content.deployments.resources[key].deploy){
+								$timeout(() => {
+									resource.scope.formData = angular.copy(alreadyFilledFormData);
+									
+									if(resource.scope.formData.deployOptions){
+										resource.deployOptions = resource.scope.formData.deployOptions;
+									}
+									
+									//if wizard, and template container only, do not show the platform picker !
+									if(currentScope.restrictions){
+										if(currentScope.restrictions.vm && (currentScope.restrictions.docker || currentScope.restrictions.kubernetes) ){
+											resource.scope.displayPlatformPicker = true;
+										}
+									}
+									else{
+										resource.scope.displayPlatformPicker = true;
+									}
+									
+									if(resource.deployOptions && resource.deployOptions.deployConfig && resource.deployOptions.deployConfig.type && resource.deployOptions.deployConfig.type === 'vm'){
+										resource.scope.updateDeploymentName(record.name, true);
+									}
+									else{
+										if(resource.scope.displayPlatformPicker){
+											resource.scope.updateDeploymentName(record.name, true);
+										}
+										resource.scope.updateCustomRepoName();
+									}
+									
+									counter++;
+									if (counter < entriesNames.length) {
+										buildMyForms(counter, cb);
+									}
+									else {
+										return cb();
+									}
+								}, 1100);
 							}
-							else {
-								return cb();
+							//external resource
+							else{
+								resource.scope.formData = angular.copy(alreadyFilledFormData);
+								
+								if(resource.scope.formData.deployOptions){
+									resource.deployOptions = resource.scope.formData.deployOptions;
+								}
+								
+								counter++;
+								if (counter < entriesNames.length) {
+									buildMyForms(counter, cb);
+								}
+								else {
+									return cb();
+								}
 							}
 						});
 					});

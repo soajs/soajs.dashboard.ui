2095841667ecd3c3786078a0abc9e9ae6470a2d7
mikehajj <mike.hajj@gmail.com>
2018-09-18 13:44:50 +0300

    changed menu order

--- modules/dashboard/infra/install.js
+++ modules/dashboard/infra/install.js
@@ -24,30 +24,6 @@ let infraNav = [
 		'ancestor': [translation.home[LANG]]
 	},
 	{
-		'id': 'infra-deployments',
-		'label': 'Deployments',
-		'checkPermission': {
-			'service': 'dashboard',
-			'route': '/infra',
-			'method': 'get'
-		},
-		'url': '#/infra-deployments',
-		'tplPath': 'modules/dashboard/infra/deployments/directives/list.tmpl',
-		'icon': 'codepen',
-		'pillar': {
-			'name': 'infra',
-			'label': "Infrastructure",
-			'position': 0
-		},
-		'hideMe': true,
-		'mainMenu': true,
-		'tracker': true,
-		'order': 2,
-		"fallbackLocation": "#/infra",
-		'scripts': ['modules/dashboard/infra/config.js', 'modules/dashboard/infra/deployments/config.js', 'modules/dashboard/infra/_services/infra.js', 'modules/dashboard/infra/deployments/controller.js'],
-		'ancestor': [translation.home[LANG]]
-	},
-	{
 		'id': 'infra-templates',
 		'label': 'Infra As Code Templates',
 		'checkPermission': {
@@ -66,9 +42,33 @@ let infraNav = [
 		'hideMe': true,
 		'mainMenu': true,
 		'tracker': true,
-		'order': 3,
+		'order': 2,
 		"fallbackLocation": "#/infra",
 		'scripts': ['modules/dashboard/infra/config.js', 'modules/dashboard/infra/iac/config.js', 'modules/dashboard/infra/_services/infra.js', 'modules/dashboard/infra/iac/services/infra.js', 'modules/dashboard/infra/iac/controller.js'],
+		'ancestor': [translation.home[LANG]]
+	},
+	{
+		'id': 'infra-deployments',
+		'label': 'Deployments',
+		'checkPermission': {
+			'service': 'dashboard',
+			'route': '/infra',
+			'method': 'get'
+		},
+		'url': '#/infra-deployments',
+		'tplPath': 'modules/dashboard/infra/deployments/directives/list.tmpl',
+		'icon': 'codepen',
+		'pillar': {
+			'name': 'infra',
+			'label': "Infrastructure",
+			'position': 0
+		},
+		'hideMe': true,
+		'mainMenu': true,
+		'tracker': true,
+		'order': 3,
+		"fallbackLocation": "#/infra",
+		'scripts': ['modules/dashboard/infra/config.js', 'modules/dashboard/infra/deployments/config.js', 'modules/dashboard/infra/_services/infra.js', 'modules/dashboard/infra/deployments/controller.js'],
 		'ancestor': [translation.home[LANG]]
 	},
 	{

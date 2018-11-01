cb94c8065f846bc03a4c19ce61d501e823715920
mikehajj <mike.hajj@gmail.com>
2018-11-01 16:28:20 +0200

    updated uiConfig for app

--- app/uiConfig.js
+++ app/uiConfig.js
@@ -67,5 +67,6 @@ var whitelistedRepos = [
 	'soajs/soajs.quick.demo',
 	'soajs/soajs.nodejs.express',
 	'soajs/soajs.nodejs.hapi',
-	'soajs/soajs.java.jaxrs_jersey'
+	'soajs/soajs.java.jaxrs_jersey',
+	'soajs/soajs.golang.mux'
 ];

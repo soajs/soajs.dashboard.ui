578bb3f35527e73f52455478857ce6ddf47d0161
antoinehage <me@antoinehage.com>
2023-10-13 06:06:29 -0400

    bumped version to 4.2.1

--- apiEndpoint.js
+++ apiEndpoint.js
@@ -38,11 +38,11 @@ if (customSettings.api && customSettings.api !== "") {
 	myDomainAPI = customSettings.api + ".";
 }
 
-// window.apiSettings = {
-// 	"api": window.location.protocol + '//' + myDomainAPI + myDomain + ":" + myPort,
-// 	"key": customSettings.key
-// };
 window.apiSettings = {
-	"api": "https://console-api.findnell.com",
-	"key": "b20c4f9a1c2b1c87c9b5d3a905d642fd114a8d6e5a5ac8b4383b1a3cc22f1889d0f319d5a4e1105d39116933a9784f8779d679a7e6cb932c6e062021585abcbc477ae2f9653624828b6323acbeb9bf986c3abc20ee832626e281eaa3da107758"
-};
+	"api": window.location.protocol + '//' + myDomainAPI + myDomain + ":" + myPort,
+	"key": customSettings.key
+}; 
+// window.apiSettings = {
+// 	"api": "https://console-api.findnell.com",
+// 	"key": "b20c4f9a1c2b1c87c9b5d3a905d642fd114a8d6e5a5ac8b4383b1a3cc22f1889d0f319d5a4e1105d39116933a9784f8779d679a7e6cb932c6e062021585abcbc477ae2f9653624828b6323acbeb9bf986c3abc20ee832626e281eaa3da107758"
+// };
